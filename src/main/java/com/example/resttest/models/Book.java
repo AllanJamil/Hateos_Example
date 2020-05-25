9307dc3ac6a988d9feaf825b84b1edd15e85c158
Allan Jamil <allanjami_91@hotmail.com>
2020-05-25 09:02:43 +0200

    intial

--- src/main/java/com/example/resttest/models/Book.java
+++ src/main/java/com/example/resttest/models/Book.java
@@ -1,14 +1,17 @@
 package com.example.resttest.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.example.resttest.controllers.BookController;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import java.io.Serializable;
 
-public class Book implements Serializable {
-    @JsonIgnore
+public class Book extends RepresentationModel implements Serializable {
     private String title;
     private String author;
-    @JsonIgnore
     private int id;
     private static final long serialVersionUID=1;
 
@@ -43,5 +46,36 @@ public class Book implements Serializable {
 
     public void setId(int id) {
         this.id = id;
+    }
+    
+    @JsonProperty
+    public Link getLink() {
+        Link link1 = null;
+        if (!this.hasLink("all_books")) {
+            link1 = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController.class).allBooks()).withRel("all_books");
+            this.add(link1);
+        }
+        return link1;
+    }
+
+    @JsonProperty
+    public Link getOtherLink() {
+
+        Link link1 = null;
+        if (!this.hasLink("delete_book")) {
+            link1 = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController.class).deleteByID(id)).withRel("delete_book");
+            this.add(link1);
+        }
+        return link1;
+    }
+
+    @JsonProperty
+    public Link getThirdLink() {
+        Link link1 = null;
+        if (!this.hasLink("add_book")) {
+            link1 = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController.class).addBook(this)).withRel("add_book");
+            this.add(link1);
+        }
+        return link1;
     }
 }
